d7a3bf21e2fd4704a3d4ba130fd859669a1c7002
Fraaxius <franthekillaz@gmail.com>
2021-05-05 15:33:40 +0000

    sdm660-common: Drop Snap Camera.
    * Using MotCamera2 by default.
    
    Co-authored-by: Deivid_21 <david.parra.ignacio@gmail.com>
    Signed-off-by: Fraaxius <franthekillaz@gmail.com>

--- common.mk
+++ common.mk
@@ -82,9 +82,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
     libbson.vendor \
-    libxml2 \
-    Snap
-
+    libxml2
 # Codec2 modules
 PRODUCT_PACKAGES += \
     com.android.media.swcodec \
