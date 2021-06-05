2203e00c0b1291679d8b2a6eb9b538d23cb334f7
electimon <electimon@gmail.com>
2021-06-05 02:32:04 -0400

    sdm660-common: Enable Blur!

--- properties.mk
+++ properties.mk
@@ -113,11 +113,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.hwc_set_default_colormode=true \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
-    debug.sf.latch_unsignaled=0 \
+    debug.sf.latch_unsignaled=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
     debug.sf.disable_backpressure=1 \
     dev.pm.dyn_samplingrate=1 \
     ro.opengles.version=196610 \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1 \
     ro.qualcomm.cabl=0
 
 # Disable iorapd
